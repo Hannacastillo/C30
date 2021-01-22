1317e59a3ad239a7bc980b101b81f5bd5638d16b
Hannacastillo <72773292+Hannacastillo@users.noreply.github.com>
2021-01-21 19:49:48 -0600

    Add files via upload

---
+++ bird.js
@@ -0,0 +1,20 @@
+class Bird extends Parent{
+
+    constructor(x,y){
+    
+    
+          super(x,y,50,50,0)
+          this.image=loadImage("sprites/bird.png")
+    
+    
+    }
+    
+    display(){
+    super.display();
+   // this.body.position.x=mouseX
+   // this.body.position.y=mouseY
+    
+    
+    }
+    
+    }
